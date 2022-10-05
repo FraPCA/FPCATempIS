bd6878fc96a3526fbea5b80a4f1a61d4d693c409
FraPCA <francesco3b@gmail.com>
2022-10-05 17:57:01 +0200

    Added class Calcolatrice, functions for add, subtract, delete and remove

---
+++ src/main/java/org/example/Calcolatrice.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Calcolatrice {
+    private int a, b;
+
+    public Calcolatrice(){
+        this.a = 0;
+        this.b = 0;
+    }
+    public int somma(int a, int b)
+    {
+        return a + b;
+    }
+    public int sottrazione(int a, int b)
+    {
+        return a - b;
+    }
+    public int moltiplicazione(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int divisione(int a, int b) throws Exception {
+        if(b != 0)
+        {
+            return a / b;
+        }
+        else
+        {
+            throw new Exception("Errore: divisione per zero.");
+        }
+    }
+}
